d599a9f62a85459fdae9097c7d82d4d302f905cf
vineet <vineetxpradhan@gmail.com>
2022-08-18 21:12:06 +0530

    search-box conditional rendering

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = (props) => {
+  const [showSearch, setShowSearch] = useState(0);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get </span>
-          <span className="search-box-container">
-            <input
-              id="search-box"
-              placeholder="What is it that you truly desire?"
-              value={props.term}
-              onChange={(e) => props.onChange(e.target.value)}
-            />
-            <img src="search_icon.svg" />
+          <span id="get-part" onClick={() => setShowSearch(1)}>
+            A place to get{" "}
           </span>
+          {showSearch ? (
+            <span className="search-box-container">
+              <input
+                id="search-box"
+                placeholder="What is it that you truly desire?"
+                value={props.term}
+                onChange={(e) => props.onChange(e.target.value)}
+              />
+              <img src="search_icon.svg" />
+            </span>
+          ) : null}
           <span> the cool stuff.</span>
         </div>
       </div>
